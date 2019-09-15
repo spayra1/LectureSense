c12e77ec6d27d96cf27ac9ab5465f074695b166e
Syamantak Payra <40369982+spayra1@users.noreply.github.com>
2019-09-14 20:23:10 -0400

    teacher student devved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import StudentView from './components/StudentView';
+import TeacherView from './components/TeacherView';
+
 function App() {
+  const [number, getNumber] = useState(0);
+
   return (
     <div className="App">
+      <div style={{display: 'flex', flexDirection: 'row', height: '100vh', width: '100%'}}>
+        <StudentView n={number} changeNumber={getNumber} />
+        <TeacherView n={number}/>
+      </div>
     </div>
   );
 }
--- src/components/StudentView.js
+++ src/components/StudentView.js
@@ -1,1 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+const StudentView = ({n, changeNumber}) => {
+
+    return (
+    <div style={{ backgroundColor: '', height: '100%', width: '50vw'}}>
+        <h3>This is student view</h3>
+        <button onClick={() => changeNumber(n + 1)}>Click me!</button>
+    </div>
+    )
+};
+
+export default StudentView;
