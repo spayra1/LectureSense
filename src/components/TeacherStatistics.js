cf853d958f9c5e391f6a6e157a0cc20e9452b0d8
Syamantak Payra <40369982+spayra1@users.noreply.github.com>
2019-09-15 04:20:18 -0400

    adjusted spacings

--- src/components/TeacherStatistics.js
+++ src/components/TeacherStatistics.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const TeacherStatistics = ({s, l, b}) => (
     <div class = "col" style={{backgroundColor: ''}}>
-        <h3>Quick Alerts:</h3>
-        <label>Slow down: {s}</label>
-        <label>Speak louder: {l}</label>
-        <label>Write bigger: {b}</label>
+        <h3>Post-Lecture Analytics:</h3>
+        <label>{(s/30*100).toFixed(2)}% of students asked you to slow down at some point in your lecture</label>
+        <label>{(l/30*100).toFixed(2)}% of students asked you to be louder</label>
+        <label>{(b/30*100).toFixed(2)}% of students asked you to write bigger on the board</label>
     </div>
 );
 
